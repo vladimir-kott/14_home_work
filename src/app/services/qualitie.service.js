1bb0f6d6bbc54d15308c89b847f90572087a21b7
Vladimir Kott <vladimirkott19@gmail.com>
2022-02-13 14:47:16 +0200

    some fix

--- src/app/services/qualitie.service.js
+++ src/app/services/qualitie.service.js
@@ -3,7 +3,7 @@ import httpService from "./http.service";
 const qualitieEndpoint = "qualities/";
 
 const qualitieService = {
-    get: async () => {
+    fetchAll: async () => {
         const { data } = await httpService.get(qualitieEndpoint);
         return data;
     }
